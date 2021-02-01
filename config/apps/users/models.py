5392643dd978557fc6d0acb76c05c06279c25495
Pranav Jandu <pranav.jandu@gmail.com>
2021-02-02 00:54:14 +0530

    Custom user model

--- config/apps/users/models.py
+++ config/apps/users/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser
+)
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    '''
+    Custom user model manager
+    '''
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    '''
+    Custom user model to use email authentication
+    '''
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    is_student = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
